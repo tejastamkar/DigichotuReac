dc9f9805d1c75ff97453fb58328275944422101e
tejastamkar <72966382+tejastamkar@users.noreply.github.com>
2021-12-14 15:04:53 +0530

    remove reelscreen btn

--- NavBar/Screen/ReelScreen.js
+++ NavBar/Screen/ReelScreen.js
@@ -4,20 +4,9 @@ import { View, Text, Button } from "react-native";
 export default function ReelScreen({ navigation }) {
   return (
     <View styles={{ flex: 1 }}>
-      <Text
-       onPress={() => {
-        navigation.navigate("Digi Chotu");
-      }}
-        style={{ fontSize: 26, fontWeight: "bold", alignSelf: "center" }}
-      >
+      <Text style={{ fontSize: 26, fontWeight: "bold", alignSelf: "center" }}>
         Reels
       </Text>
-      <Button
-        title="Press me"
-        onPress={() => {
-          navigation.navigate("Review");
-        }}
-      />
     </View>
   );
 }
